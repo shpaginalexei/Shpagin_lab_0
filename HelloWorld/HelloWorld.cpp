ae1d120a9b8ee0cec28b65257383e8cb97334305
shpaginalexei <shpagin05@gmail.com>
2023-09-03 17:35:56 +0300

    Add "HelloWorld"

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n" << endl;
+
+    int a = 0;
+    cout << "a: ";
+    cin >> a;
+    cout << "You entered " << a << endl;
+}
+
